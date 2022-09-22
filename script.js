b6a27e4a4d0936237fdabaff92999587fed57caa
Mariskr <97429242+Mariskr@users.noreply.github.com>
2022-09-22 13:06:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 async function iegutDarglietasNoApi(){ //darbiba nenotiek veina pec otras un var notikt cita laika
-let datiNoApi=await fetch ("https://fakestoreapi.com/products")//dati bus jagaida
+let datiNoApi=await fetch ("https://fakestoreapi.com/products/category/electronics")//dati bus jagaida
 let datiJson=datiNoApi.json;
-    console.log(datiJson);
-
+console.log(datiJson);
 }
